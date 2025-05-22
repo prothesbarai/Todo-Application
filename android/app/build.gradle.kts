178c0858ae30e95ae81e682f9ba1f22c51fa54da
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-05-22 11:04:36 +0600

    v1.0.0.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.prothesbarai.notepad.todoapps"
+    namespace = "com.prothesbari.notepad"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.prothesbarai.notepad.todoapps"
+        applicationId = "com.prothesbari.notepad"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/prothesbari/notepad/MainActivity.kt
+++ android/app/src/main/kotlin/com/prothesbari/notepad/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.prothesbarai.notepad.todoapps
+package com.prothesbari.notepad
 
 import io.flutter.embedding.android.FlutterActivity
 
