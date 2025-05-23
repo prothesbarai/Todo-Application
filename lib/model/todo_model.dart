9bea730867ab5e1485c0d247815029f39dbb1911
prothesbarai <aspprothesbarai1619@gmail.com>
2025-05-23 21:24:55 +0600

    v1.0.5

---
+++ lib/model/todo_model.dart
@@ -0,0 +1,14 @@
+class TodoModel {
+
+  final String text;
+  final bool isComplete;
+  final bool isSelectAll;
+
+  TodoModel({
+    required this.text,
+    required this.isComplete,
+    required this.isSelectAll
+  });
+
+
+}
